40fdb321fd803ac399a13e94ed6b3d24dfb3ae13
Kenta Kodashima <kentakodashima@gmail.com>
2018-10-05 01:07:02 -0700

    Done with Day5. Modified the README

---
+++ Challenge5.js
@@ -0,0 +1,44 @@
+/*
+## Challenge #5
+Since our GRID is written in coordinates like A3 or D8, we need code that is going to be able to convert 
+those coordinates to the numbers we need to fetch data out of the GRID array(s). All of the lighthouse 
+operators have reported in and we know that their areas never go above Z.
+
+Let's build a function called convertColumn() which, when given the coordinate as an argument, will return 
+the number of the column.
+
+For example, calling convertColumn('C4') should return 2 as the 'C' column is the third column, and since 
+JavaScript arrays start at 0, that would make 2 the third column.
+*/
+
+function countRows() {
+  let rowsNum = GRID.length
+  return rowsNum
+}
+
+function countColumns() {
+  let rowsNum = countRows()
+  let colNum = GRID[rowsNum - 1].length
+  return colNum
+}
+
+function gridSize() {
+  let rowCount = countRows()
+  let colCount = countColumns()
+
+  return "" + colCount + " x " + rowCount
+}
+
+function totalCells() {
+  let rowCount = countRows()
+  let colCount = countColumns()
+
+  return rowCount * colCount
+}
+
+function convertColumn(coordinate) {
+  let alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"]
+  
+  return alphabets.indexOf(coordinate[0])
+}
+console.log(convertColumn('C4'))
